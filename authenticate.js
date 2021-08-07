8ef35c9163b35787804e2d2c3adda577681ec6be
Sasarez <Lehmanc24@gmail.com>
2021-08-07 12:39:16 -0700

    Workshop Week 3

--- authenticate.js
+++ authenticate.js
@@ -1,19 +1,18 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken");
 
-const config = require('./config.js')
+const config = require("./config.js");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
-
+exports.getToken = function (user) {
+  return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
 
 const opts = {};
@@ -21,24 +20,27 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassword = passport.use(
-    new JwtStrategy(
-        opts,
-        (jwt_payload, done) => {
-            console.log('JWT payload:', jwt_payload);
-            User.findOne({_id: jwt_payload._id}, (err, user) => {
-                if (err) {
-                    return done(err, false);
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("JWT payload:", jwt_payload);
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
+      if (err) {
+        return done(err, false);
+      } else if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
 
-                }
-                    else if (user) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false);
-                    }
-                }
-            )
-        }
-    )
-)
-
-exports.verifyUser = passport.authenticate('jwt', {session: false})
+exports.verifyUser = passport.authenticate("jwt", { session: false });
+exports.verifyAdmin = (req, res, next) => {
+  if (req.user.admin) {
+    return next();
+  } else {
+    const err = new Error("You are not authorized to perform this operation!");
+    err.status = 403;
+    return next(err);
+  }
+};
