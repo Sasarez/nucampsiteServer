5e03ac286df08ee53d59eafe44d7e62b68a54557
Sasarez <Lehmanc24@gmail.com>
2021-08-11 16:51:57 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': 'pieholebananafacebombingruination',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '571074583902938',
+        clientSecret: 'fd27e7349bc0c07fe95e70bcde6d84d3'
+    }
 }
