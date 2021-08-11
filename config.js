248f19bdc1bb554ae807f1a3bd958f1e9fc9da3c
Sasarez <Lehmanc24@gmail.com>
2021-08-11 16:56:11 -0700

    trying to remove config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': 'pieholebananafacebombingruination',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '571074583902938',
-        clientSecret: 'fd27e7349bc0c07fe95e70bcde6d84d3'
-    }
-}
